c34cb9b9345ef56afc44b9b0a8acdf9177b22587
gibcheesepuffs <shou@disroot.org>
2021-06-22 01:51:29 +0530

    helpers: Remove nfc related packages

--- helpers/build_packages.sh
+++ helpers/build_packages.sh
@@ -284,10 +284,6 @@ if [ "$BUILDMW" = "1" ]; then
             buildmw -u "https://github.com/mer-hybris/libgbinder-radio" || die
             buildmw -u "https://github.com/mer-hybris/bluebinder" || die
             buildmw -u "https://github.com/mer-hybris/ofono-ril-binder-plugin" || die
-            buildmw -u "https://git.sailfishos.org/mer-core/nfcd.git" || die
-            buildmw -u "https://github.com/mer-hybris/libncicore.git" || die
-            buildmw -u "https://github.com/mer-hybris/libnciplugin.git" || die
-            buildmw -u "https://github.com/mer-hybris/nfcd-binder-plugin" || die
         fi
         buildmw -u "https://github.com/mer-hybris/pulseaudio-modules-droid.git" \
                 -s rpm/pulseaudio-modules-droid.spec || die
